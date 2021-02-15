22e4ea6cb524eb0d70310ac7d699a791d763bccc
Marko Tasic <mtasic85@gmail.com>
2021-02-15 14:03:04 +0100

    fixed indent of fetch and example-fetch-0.js

--- example-fetch-0.js
+++ example-fetch-0.js
@@ -3,28 +3,17 @@ import fetch from './fetch.js';
 let id = Math.random() * 1000000 | 0;
 
 fetch('http://localhost:3000/posts', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ 'id': id, 'value': id + '0' }) })
-    .then(data => {
-        // console.log('data [text]:' + data.text());
-        // console.log('data [json]:' + JSON.stringify(data.json()));
-        return data.json();
-    })
-    .then(res => {
-        console.log('res[0]: ' + JSON.stringify(res));
+  .then(res => res.json())
+  .then(data => {
+    console.log('data[0]: ' + JSON.stringify(data));
 
-        return fetch(`http://localhost:3000/posts/${id}`)
-            .then(data => {
-                // console.log('data [text]:' + data.text());
-                // console.log('data [json]:' + JSON.stringify(data.json()));
-                return data.json();
-            })
-            .then(res => {
-                console.log('res[1]: ' + JSON.stringify(res));
-            })
-            .catch(err => {
-                console.log('err[1]: ' + JSON.stringify(err));
-            });
-
-    })
-    .catch(err => {
-        console.log('err[0]: ' + JSON.stringify(err));
-    });
+    return fetch(`http://localhost:3000/posts/${id}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log('data[1]: ' + JSON.stringify(data));
+      });
+  })
+  .catch(err => {
+    console.log('err: ' + JSON.stringify(err));
+    console.log(err.stack);
+  });
--- fetch.js
+++ fetch.js
@@ -1,42 +1,45 @@
 import { popen } from 'std';
 
-export default function fetch(resource, init) {
-    init = init || {
-        method: 'GET',
-        headers: null,
-        body: null,
+function fetchPollyfil(resource, init) {
+  init = init || {
+    method: 'GET',
+    headers: null,
+    body: null,
+  };
+  
+  let curlCmd = `curl -s -X${init.method} "${resource}"`;
+
+  if (init.headers != null) {
+    curlCmd = `${curlCmd} ${Object.entries(init.headers).map(n => `-H '${n[0]}: ${n[1]}'`).join(' ')}`
+  }
+
+  if (init.method != 'GET') {
+    curlCmd = `${curlCmd} -d '${init.body}'`
+  }
+
+  const spErr = {};
+  const sp = popen(curlCmd, 'r', spErr);
+  const curlOutput = sp.readAsString();
+  const responseUrl = resource;
+  const responseHeaders = {};
+  let responseOk = true;
+  let responseStatus = 200;
+
+  const p = new Promise((resolve, reject) => {
+    const response = {
+      headers: responseHeaders,
+      ok: responseOk,
+      url: responseUrl,
+      text: () => curlOutput,
+      json: () => JSON.parse(curlOutput),
     };
-    
-    let spErr = {};
-    let curlCmd = `curl -s -X${init.method} "${resource}"`;
 
-    if (init.headers != null) {
-        curlCmd = `${curlCmd} ${Object.entries(init.headers).map(n => `-H '${n[0]}: ${n[1]}'`).join(' ')}`
-    }
+    resolve(response);
+  });
 
-    if (init.method != 'GET') {
-        curlCmd = `${curlCmd} -d '${init.body}'`
-    }
+  return p;
+}
 
-    // console.log(curlCmd);
-    let sp = popen(curlCmd, 'r', spErr);
-    let curlOutput = sp.readAsString();
-    let responseUrl = resource;
-    let responseHeaders = {};
-    let responseOk = true;
-    let responseStatus = 200;
+globalThis.fetch = fetchPollyfil;
 
-    let p = new Promise((resolve, reject) => {
-        let response = {
-            headers: responseHeaders,
-            ok: responseOk,
-            url: responseUrl,
-            json: () => JSON.parse(curlOutput),
-            text: () => curlOutput,
-        };
-
-        resolve(response);
-    });
-
-    return p;
-};
+export default fetchPollyfil;
